30a23ddf75d6ad70e404d4b1ef22d87517e43d30
zealprajapati <zeelprajapati18@gmail.com>
2020-02-12 17:26:53 -0500

    first commit

---
+++ main (2).cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  AI_GridWorld
+//
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "VisualDisplay.h"
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, const char * argv[]) {
+
+	VisualDisplay display;
+	display.loadImages();
+	display.run();
+
+
+	return 0;
+}
